00243ca798cb0e058d8db763e1c4612297ab0b9b
Kseniia Astakhova <astakhova.ksyusha.07@mail.ru>
2024-04-10 00:17:04 +0300

    dummy api prediction

--- api/storage.py
+++ api/storage.py
@@ -1,6 +1,7 @@
 import boto3
 import io
 import torch
+import pandas as pd
 
 session = boto3.session.Session()
 s3 = session.client(
@@ -9,8 +10,20 @@ s3 = session.client(
 )
 
 def get_model():
-    get_object_response = s3.get_object(Bucket='studcamp-ml', Key='model.pth')
-    file = get_object_response['Body'].read()
+    response = s3.get_object(Bucket='studcamp-ml', Key='catboost.pth')
+    file = response['Body'].read()
     buffer = io.BytesIO(file)
     model = torch.load(buffer)
     return model
+
+def get_imoex():
+    response = s3.get_object(Bucket='studcamp-ml', Key='IMOEX_filled.csv')
+    csv_string = response['Body'].read().decode('utf-8')
+    df = pd.read_csv(io.StringIO(csv_string))
+    return df
+
+def get_news():
+    response = s3.get_object(Bucket='studcamp-ml', Key='finam_news_scored.csv')
+    csv_string = response['Body'].read().decode('utf-8')
+    df = pd.read_csv(io.StringIO(csv_string))
+    return df
