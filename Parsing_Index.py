7f6f1f4d57300909a9377eabb55f3778476d30c4
mchomak <72063735+mchomak@users.noreply.github.com>
2024-04-10 00:31:44 +0300

    Add files via upload
    Обновил парсеры, теперь можно парсить отдельно цену открытия индекса за сегодня и сразу обновлять базу данных, + оба парсера запихнул в одни файл (Parsing_Index), скорее всего так будет удобнее

--- Parsing_Index.py
+++ Parsing_Index.py
@@ -1,6 +1,5 @@
 import requests
 from datetime import datetime, timedelta
-import pandas as pd
 import os
 import shutil
 from selenium import webdriver
@@ -9,15 +8,49 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 import time
+import pandas as pd
+from bs4 import BeautifulSoup
+
+
+def find_downloaded_file(download_folder, prefix):
+    """
+    Ищет файл с заданным префиксом в папке загрузок.
+    
+    :param prefix: Префикс искомого файла.
+    :return: Полный путь к файлу или None, если файл не найден.
+    """
+    for filename in os.listdir(download_folder):
+        if filename.startswith(prefix):
+            return os.path.join(download_folder, filename)
+    return None
+
+
+def update_data(self, open, today):
+    file_path = find_downloaded_file(self.download_folder, self.prefix)
+    if file_path:
+        print(f"Найден файл: {file_path}")
+        df = pd.read_csv(file_path)
+        if today not in df["DATE"].values:
+            df.loc[len(df)] = [today, open]
+            df.to_csv(file_path, index=False)
+
+        print(f"Данные успешно обнавлены")
+        
+    else:
+        self.get_history_data()
+        self.get_open_price()
+
 
 class IMOEXDataDownloader:
-    def __init__(self, start_date, end_date=datetime.now().strftime('%Y-%m-%d')):
+    def __init__(self, start_date, end_date=datetime.now().strftime('%Y-%m-%d'), download_folder = os.path.join(os.getcwd(), "ml", "data"), prefix = "IMOEX"):
+        self.download_folder = download_folder
         self.start_date = start_date
         self.end_date = end_date
         self.born_date = datetime.strptime("2004-01-01", '%Y-%m-%d')
         self.main_path = "ml/data"
         self.unclearn_path = os.path.join(self.main_path, "UnClearn")
         self.clearn_path = os.path.join(self.main_path, "Clearn")
+        self.prefix = "IMOEX"
 
 
     def get_history_data(self):
@@ -136,7 +169,7 @@ class IMOEXDataDownloader:
             print(f"Не удалось удалить папку '{path}'. Причина: {e}")
 
 
-    def get_open_imoex(self):
+    def get_open_price(self):
         """
         Забирает цену открытия с мосБиржи.
         """
@@ -145,17 +178,94 @@ class IMOEXDataDownloader:
             driver.get('https://ru.tradingview.com/symbols/MOEX-IMOEX/')
             time.sleep(1)
             price_elements = driver.find_elements(By.CSS_SELECTOR, '.apply-overflow-tooltip.value-GgmpMpKr')
-            price = price_elements[2].text  
-            price = str(price).replace("RUB", "")
-            price = float(price)
+            open = price_elements[2].text  
+            open = str(open).replace("RUB", "")
+            open = float(open)
             
         finally:
             driver.quit()
         
         today = datetime.now().strftime('%Y-%m-%d')
+        update_data(self, open, today)
 
 
-if __name__ == "__main__":
-    downloader = IMOEXDataDownloader(start_date="2009-01-01", end_date="2024-04-08")
-    downloader.get_history_data()
+class SPBEDataDownloader:
+    def __init__(self, download_folder = os.path.join(os.getcwd(), "ml", "data"), prefix = "SPBIRUS"):
+        self.download_folder = download_folder
+        self.prefix = prefix
+        self.url = "https://spbexchange.ru/stocks/indices/SPBIRUS/"
+        self.response = requests.get(self.url)
+        self._setup_chrome_driver()
+
+
+    def _setup_chrome_driver(self):
+        """
+        Настраивает драйвер Chrome с указанными опциями.
+        """
+        chrome_options = Options()
+        prefs = {"download.default_directory": self.download_folder}
+        chrome_options.add_experimental_option("prefs", prefs)
+        service = Service(ChromeDriverManager().install())
+        self.driver = webdriver.Chrome(service=service, options=chrome_options)
+
+
+    def get_history_data(self):
+        """
+        Парсит и сохраняет архивные данные с 2015-11-25 по вчерашний день.
+        """
+        self._download_data()
+        file_path = find_downloaded_file(self.download_folder, self.prefix)
+        if file_path:
+            print(f"Найден файл: {file_path}")
+            self._process_downloaded_file(file_path)
+        else:
+            print("Файл не найден.")
+
+
+    def _download_data(self):
+        """
+        Загружает данные, кликая по кнопке на странице.
+        
+        :param url: URL страницы для загрузки.
+        """
+        self.driver.get(self.url)
+        try:
+            button_selector = '.Button_root__ZhrsR.Button_spb__tqqde.Button_contained__7HNa2.Button_cancel__5lME4.Button_sm__ndTMz'
+            self.driver.find_element(By.CSS_SELECTOR, button_selector).click()
+            time.sleep(2)  # Ожидание загрузки файла
+        finally:
+            self.driver.quit()
+
+
+    def _process_downloaded_file(self, file_path):
+        """
+        Обрабатывает скачанный файл: читает и сохраняет его с новой структурой.
+        
+        :param file_path: Полный путь к файлу для обработки.
+        """
+        df = pd.read_csv(file_path, sep=';', decimal=',')
+        df.columns = ["DATE", "OPEN"]
+        df.to_csv(file_path, index=False)
+
+
+    def get_open_price(self):
+        """
+        Забирает цену открытия с Питерской Биржи и обновляет данные в архивных значениях.
+        """
+        html_content = self.response.text
+        
+        soup = BeautifulSoup(html_content, 'html.parser')
+        price_elements = soup.select('.IndexesCard_value__cjTBc')
+
+        open = float(price_elements[0].text.replace(',', '.').replace(' ', ''))
+        today = datetime.now().strftime('%Y-%m-%d')
+
+        update_data(self, open, today)
+        
     
+if __name__ == "__main__":
+    imoex_parser = IMOEXDataDownloader(start_date="2009-01-01", end_date="2024-04-08")
+    imoex_parser.get_open_price()
+
+    spb_parser = SPBEDataDownloader()
+    spb_parser.get_open_price()
--- Parsing_SPB.py
+++ Parsing_SPB.py
@@ -6,67 +6,112 @@ from selenium.webdriver.chrome.options import Options
 from time import sleep
 import pandas as pd
 import os
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
 
-def setup_chrome_driver(download_folder):
-    """
-    Настраивает драйвер Chrome с указанными опциями.
-    
-    :param download_folder: Папка для загрузки файлов.
-    :return: Инициализированный драйвер.
-    """
-    chrome_options = Options()
-    prefs = {"download.default_directory": download_folder}
-    chrome_options.add_experimental_option("prefs", prefs)
-    service = Service(ChromeDriverManager().install())
-    return webdriver.Chrome(service=service, options=chrome_options)
+class SPBEDataDownloader:
+    def __init__(self, download_folder = os.path.join(os.getcwd(), "ml", "data"), prefix = "SPBIRUS"):
+        self.download_folder = download_folder
+        self.prefix = prefix
+        self.url = "https://spbexchange.ru/stocks/indices/SPBIRUS/"
+        self.response = requests.get(self.url)
+        self._setup_chrome_driver()
 
-def download_data(driver, url):
-    """
-    Загружает данные, кликая по кнопке на странице.
-    
-    :param driver: Драйвер браузера.
-    :param url: URL страницы для загрузки.
-    """
-    driver.get(url)
-    try:
-        button_selector = '.Button_root__ZhrsR.Button_spb__tqqde.Button_contained__7HNa2.Button_cancel__5lME4.Button_sm__ndTMz'
-        driver.find_element(By.CSS_SELECTOR, button_selector).click()
-        sleep(2)  # Ожидание загрузки файла
-    finally:
-        driver.quit()
 
-def find_downloaded_file(download_folder, prefix):
-    """
-    Ищет файл с заданным префиксом в папке загрузок.
-    
-    :param download_folder: Папка для поиска.
-    :param prefix: Префикс искомого файла.
-    :return: Полный путь к файлу или None, если файл не найден.
-    """
-    for filename in os.listdir(download_folder):
-        if filename.startswith(prefix):
-            return os.path.join(download_folder, filename)
-    return None
+    def _setup_chrome_driver(self):
+        """
+        Настраивает драйвер Chrome с указанными опциями.
+        """
+        chrome_options = Options()
+        prefs = {"download.default_directory": self.download_folder}
+        chrome_options.add_experimental_option("prefs", prefs)
+        service = Service(ChromeDriverManager().install())
+        self.driver = webdriver.Chrome(service=service, options=chrome_options)
 
-def process_downloaded_file(file_path):
-    """
-    Обрабатывает скачанный файл: читает и сохраняет его с новой структурой.
-    
-    :param file_path: Полный путь к файлу для обработки.
-    """
-    df = pd.read_csv(file_path, sep=';', decimal=',')
-    df.columns = ["DATE", "OPEN"]
-    df.to_csv(file_path, index=False)
+
+    def get_history_data(self):
+        """
+        Парсит и сохраняет архивные данные с 2015-11-25 по вчерашний день.
+        """
+        self._download_data()
+        file_path = self._find_downloaded_file(self.prefix)
+        if file_path:
+            print(f"Найден файл: {file_path}")
+            self._process_downloaded_file(file_path)
+        else:
+            print("Файл не найден.")
+
+
+    def _download_data(self):
+        """
+        Загружает данные, кликая по кнопке на странице.
+        
+        :param url: URL страницы для загрузки.
+        """
+        self.driver.get(self.url)
+        try:
+            button_selector = '.Button_root__ZhrsR.Button_spb__tqqde.Button_contained__7HNa2.Button_cancel__5lME4.Button_sm__ndTMz'
+            self.driver.find_element(By.CSS_SELECTOR, button_selector).click()
+            sleep(2)  # Ожидание загрузки файла
+        finally:
+            self.driver.quit()
+
+
+    def _find_downloaded_file(self, prefix):
+        """
+        Ищет файл с заданным префиксом в папке загрузок.
+        
+        :param prefix: Префикс искомого файла.
+        :return: Полный путь к файлу или None, если файл не найден.
+        """
+        for filename in os.listdir(self.download_folder):
+            if filename.startswith(prefix):
+                return os.path.join(self.download_folder, filename)
+        return None
+
+
+    def _process_downloaded_file(self, file_path):
+        """
+        Обрабатывает скачанный файл: читает и сохраняет его с новой структурой.
+        
+        :param file_path: Полный путь к файлу для обработки.
+        """
+        df = pd.read_csv(file_path, sep=';', decimal=',')
+        df.columns = ["DATE", "OPEN"]
+        df.to_csv(file_path, index=False)
+
+
+    def get_open_price(self):
+        """
+        Забирает цену открытия с Питерской Биржи и обновляет данные в архивных значениях.
+        """
+        html_content = self.response.text
+        
+        soup = BeautifulSoup(html_content, 'html.parser')
+        price_elements = soup.select('.IndexesCard_value__cjTBc')
+
+        open = float(price_elements[0].text.replace(',', '.').replace(' ', ''))
+        today = datetime.now().strftime('%Y-%m-%d')
+
+        file_path = self._find_downloaded_file(self.prefix)
+        if file_path:
+            print(f"Найден файл: {file_path}")
+            df = pd.read_csv(file_path)
+            if today not in df["DATE"]:
+                df.loc[len(df)] = [today, open]
+                df.to_csv(file_path, index=False)
+
+            print(f"Данные успешно обнавлены")
+            
+        else:
+            self.get_history_data()
+            self.get_open_price()
+        
+
 
 if __name__ == "__main__":
-    download_folder = os.path.join(os.getcwd(), "ml", "data")
-    url = "https://spbexchange.ru/stocks/indices/SPBIRUS/"
-    driver = setup_chrome_driver(download_folder)
-    download_data(driver, url)
-    prefix = "SPBIRUS"
-    full_path = find_downloaded_file(download_folder, prefix)
-    if full_path:
-        print(f"Найден файл: {full_path}")
-        process_downloaded_file(full_path)
-    else:
-        print("Файл не найден.")
+        download_folder = os.path.join(os.getcwd(), "ml", "data")
+        parser = SPBEDataDownloader()
+        parser.get_open_price()
+    
