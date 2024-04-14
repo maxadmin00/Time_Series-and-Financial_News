46cec5c454647bd046662595388ed6c3224551ba
gleb4ik177 <glebolymp@gmail.com>
2024-04-14 21:06:59 +0300

    remove unused files

--- DAGs/test.py
+++
@@ -1,5 +0,0 @@
-from news_parser import Scrapper
-scr = Scrapper('https://www.finam.ru/publications/section/market/date/', '2024-04-08', '2024-04-08')
-scr.parse()
-scr.estimate()
-print(scr.main_df)
